9c6d9a1df37a90469fad635fe55dd3591032390b
surrsoft <evgebar-1@mail.ru>
2025-10-14 08:44:13 +0300

    ports config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 22155,      // порт для dev (по умолчанию 5173)
+    strictPort: true // если true — не переключается на следующий свободный
+  },
+  preview: {
+    port: 22156   // порт для vite preview (по умолчанию 4173)
+  }
 })
